4e818d78b3031c63715847b654a164109359e7a5
Charuka Karunanayake <charukakarunanayake@gmail.com>
2024-11-06 22:15:57 +0530

    Add initial support with parsing alpha numeric

--- parser/lexer/lexer.go
+++ parser/lexer/lexer.go
@@ -28,6 +28,17 @@ func (lx *Lexer) NextToken() (tok token.Token) {
 	case '@':
 		tok = newToken(token.IDENT_PREFIX, token.LiteralMap[token.IDENT_PREFIX])
 	default:
+		if isAlphaNumeric(lx.ch) {
+			value := lx.getAlphaNumericValue()
+
+			if len(value) > 0 {
+				if tt, ok := token.KeywordMap[value]; ok {
+					tok = newToken(tt, value)
+				} else {
+					tok = newToken(token.VALUE, value)
+				}
+			}
+		}
 	}
 
 	lx.readChar()
@@ -35,11 +46,26 @@ func (lx *Lexer) NextToken() (tok token.Token) {
 	return
 }
 
+func (lx *Lexer) getAlphaNumericValue() string {
+	var val string
+
+	for isAlphaNumeric(lx.ch) {
+		val += string(lx.ch)
+		lx.readChar()
+	}
+
+	return val
+}
+
 func newToken(tt token.TokenType, literal string) token.Token {
 	return token.Token{
 		Type:    tt,
 		Literal: literal,
 	}
+}
+
+func isAlphaNumeric(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')
 }
 
 func (lx *Lexer) skipWhiteSpaces() {
--- parser/token/token.go
+++ parser/token/token.go
@@ -11,12 +11,13 @@ const (
 	ILLEGAL TokenType = iota // tokens or characters that can't be figured out
 	EOF                      // let the parser know when to stop
 
-	IDENT // identifires (variable names)
+	VALUE // identifires (variable names)
 
 	ASSIGN       // assignment operator `=`
 	IDENT_PREFIX // variable declaration prefix `@`
 	LBRACE       // value insert start `{{`
 	RBRACE       // value insert start `}}`
+	COLON
 
 	ESC_CHAR // escape character `\`
 
@@ -38,6 +39,7 @@ var LiteralMap = map[TokenType]string{
 	LBRACE:       "{{",
 	RBRACE:       "}}",
 	ESC_CHAR:     "\\",
+	COLON:        ":",
 	POST:         "POST",
 	GET:          "GET",
 	PUT:          "PUT",
@@ -47,4 +49,16 @@ var LiteralMap = map[TokenType]string{
 	CONNECT:      "CONNECT",
 	OPTIONS:      "OPTIONS",
 	TRACE:        "TRACE",
+}
+
+var KeywordMap = map[string]TokenType{
+	"POST":    POST,
+	"GET":     GET,
+	"PUT":     PUT,
+	"DELETE":  DELETE,
+	"PATCH":   PATCH,
+	"HEAD":    HEAD,
+	"CONNECT": CONNECT,
+	"OPTIONS": OPTIONS,
+	"TRACE":   TRACE,
 }
