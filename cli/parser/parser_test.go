103c5dbc4f639d79acf89f13971ac7b68dfa2839
CharukaK <charukakarunanayake@gmail.com>
2025-01-08 22:57:39 +0530

    Add assertions to unit test

--- cli/parser/parser_test.go
+++ cli/parser/parser_test.go
@@ -1,9 +1,9 @@
 package parser
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/CharukaK/request-monkey/cli/ast"
 	"github.com/CharukaK/request-monkey/cli/lexer"
 )
 
@@ -14,7 +14,24 @@ func TestVarDecl(t *testing.T) {
 
     p.Parse()
     
-    fmt.Println(p.document)
+    document := p.document
+
+
+    if len(document.Statements) != 1 {
+        t.Fatal("mismatch on number of statements")
+    }
+
+    stmt, ok := document.Statements[0].(*ast.Variable)
+
+    if !ok {
+        t.Fatal("invalid type found for statement")
+    }
+
+    if stmt.Name.String() != "hello" || stmt.Value.String() != "asdfkjasdlfkj" {
+        t.Fatal("invalid name and value combintaion detected")
+    }
+
+
 
 	// for _, tc := range testcases {
 	// 	tok := l.NextToken()
